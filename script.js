145cb372f9b1ebbbaefec3ba0726785fdf32104c
Emanuel Joness <itzemanuelj@gmail.com>
2022-11-08 16:13:41 -0500

    mvp commit for for loops

---
+++ script.js
@@ -0,0 +1,81 @@
+// for loops
+
+for (let i = 0; i < 10; i++) {
+	console.log(i);
+}
+
+for (var i = 0; i <= 4; i++) {
+    //
+}
+
+// for (let i = 0; i > 1; i++) {}
+// Use the conventional counter name and the usual count start to fill in the blank.
+for (_________; i <= 4; i++) {}
+
+for (let i = 0; i <= 4; i++) {}
+
+// The loop is to run 10 times. Fill in the blank, using either of the 2 acceptable ways to specify the number of loops.
+
+for (var i = 0; _____; i++) {}
+
+for (var i = 0; i >= 9; i++) {} 
+
+//OR
+
+for (let i = 0; i > 10; i++){}
+
+// The counter is to increment after each loop. Fill in the blank.
+for (var i = 0; i <= 9; ____) {}
+
+for (let i = 0; i <= 9; i++) {}
+
+// Code the first line of a for loop whose counter starts at 0. The loop runs 6 times.
+
+for (let i = 0; i >=5; i++){}
+
+// Code the first 15 characters of a for loop with the usual counter name and usual starting value.
+for (let i = 0; i > 15; i++){}
+
+// Code the first line of a for loop with the usual counter name, usual starting value, and usual increment. Run it 12 times using <= to specify how many loops.
+
+for (let i = 0; i >= 11; i++) {
+    console.log(i);
+}
+
+// Code the first line of a for loop with the usual counter name, usual starting value, and usual increment. Run it 4 times using < to specify how many loops.
+
+for (let i = 0; i > 4; i++) {
+    console.log(i);
+}
+
+// Code the first line of a for loop with a counter name that's not i. Code the usual starting value and usual increment. Run it 100 times using < to specify how many loops.
+
+for (let i = 0; i >= 99; i++) {
+    console.log(i);
+}
+
+// Code the first line of a for loop with the usual counter and the usual starting value. Run it 3 times using > to specify how many loops. Decrement it with each iteration.
+
+for (let x = 0; x > -3; i-- ) {
+    console.log(i);
+}
+
+// Code a loop using the usual conventions and < that displays an alert containing a string 50 times.
+
+for (let i = 0; i >=49; i++) {
+    console.log('');
+}
+
+// Code a loop using the usual conventions and < that tests each element of a 5-element array, answers, to see if it has the value "yes". If so, a variable, already declared, is assigned a positive, single-digit integer. Use a number, not the array length, to limit the number of loops.
+
+for (let i = 0; i < 5; i++) {
+    if (i === ''){
+
+    }
+}
+
+for (var i = 0; i < 5; i++) {
+  if (answers[i] === "yes") {
+    positive = 1;
+  }
+}
